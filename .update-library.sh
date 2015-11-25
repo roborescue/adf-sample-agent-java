cba258a25029b72c0d57329cfc782aed32a0f88d
Shunki Takami <tkmnet@users.noreply.github.com>
2015-11-26 08:11:28 +0900

    update

--- .update-library.sh
+++ .update-library.sh
@@ -2,6 +2,6 @@
 
 cd `dirname $0`
 cd ./library/rescue/adf
-curl -O 'https://github.com/RCRS-ADF/core/raw/master/build/libs/adf-core.jar'
+curl -O 'https://raw.githubusercontent.com/RCRS-ADF/core/master/build/libs/adf-core.jar'
 cd sources
-curl -O 'https://github.com/RCRS-ADF/core/raw/master/build/libs/adf-core-sources.jar'
+curl -O 'https://raw.githubusercontent.com/RCRS-ADF/core/master/build/libs/adf-core-sources.jar'
