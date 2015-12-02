35f3d2b9f2a069238e3fc3c65e838a74da0ff4cd
Shunki <tkmnet@users.noreply.github.com>
2015-12-02 13:42:29 +0900

    Update .update-library.sh

--- .update-library.sh
+++ .update-library.sh
@@ -1,11 +1,26 @@
 #!/bin/sh
 
+WGET='wget --no-check-certificate -O'
+if ! [ -x `which wget||echo /dev/null` ]; then
+	if [ -x `which curl||echo /dev/null` ]; then
+		WGET='curl -o'
+	else
+		if ! [ -x `which apt-get||echo /dev/null` ]; then
+			echo "[!] This script repuire apt-get or wget."
+			exit
+		fi
+		sudo apt-get install -y wget
+		sh -c "sh $0"
+		exit
+	fi
+fi
+
 cd `dirname $0`
 cd ./library/rescue/adf
 # core
-curl -o 'adf-core.jar' "https://raw.githubusercontent.com/RCRS-ADF/core/master/build/libs/adf-core.jar?$$"
+$WGET 'adf-core.jar' "https://raw.githubusercontent.com/RCRS-ADF/core/master/build/libs/adf-core.jar?$$"
 # modules
-curl -o 'adf-modules.jar' "https://raw.githubusercontent.com/RCRS-ADF/modules/master/build/libs/adf-modules.jar?$$"
+$WGET 'adf-modules.jar' "https://raw.githubusercontent.com/RCRS-ADF/modules/master/build/libs/adf-modules.jar?$$"
 cd sources
-curl -o 'adf-core-sources.jar' "https://raw.githubusercontent.com/RCRS-ADF/core/master/build/libs/adf-core-sources.jar?$$"
-curl -o 'adf-modules-sources.jar' "https://raw.githubusercontent.com/RCRS-ADF/modules/master/build/libs/adf-modules-sources.jar?$$"
+$WGET 'adf-core-sources.jar' "https://raw.githubusercontent.com/RCRS-ADF/core/master/build/libs/adf-core-sources.jar?$$"
+$WGET 'adf-modules-sources.jar' "https://raw.githubusercontent.com/RCRS-ADF/modules/master/build/libs/adf-modules-sources.jar?$$"
