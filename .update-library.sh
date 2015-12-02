fc1c3f4f46546758b4f2c990ddb468ba7b9f960c
Kazuo Takayanagi <URANOS-AIT@users.noreply.github.com>
2015-12-02 12:51:50 +0900

    add modules-jar

--- .update-library.sh
+++ .update-library.sh
@@ -2,6 +2,10 @@
 
 cd `dirname $0`
 cd ./library/rescue/adf
+# core
 curl -o 'adf-core.jar' "https://raw.githubusercontent.com/RCRS-ADF/core/master/build/libs/adf-core.jar?$$"
+# modules
+curl -o 'adf-modules.jar' "https://raw.githubusercontent.com/RCRS-ADF/modules/master/build/libs/adf-modules.jar?$$"
 cd sources
 curl -o 'adf-core-sources.jar' "https://raw.githubusercontent.com/RCRS-ADF/core/master/build/libs/adf-core-sources.jar?$$"
+curl -o 'adf-modules-sources.jar' "https://raw.githubusercontent.com/RCRS-ADF/modules/master/build/libs/adf-modules-sources.jar?$$"
