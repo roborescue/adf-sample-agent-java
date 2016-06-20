0a503a2ca7e2d666c0c67ce40a66633a467f0eb5
URANOS-AIT <uranos.maslab@gmail.com>
2016-06-20 15:42:22 +0900

    bug fix

---
+++ src/adf/sample/extaction/ActionTransport2.java
@@ -0,0 +1,83 @@
+package adf.sample.extaction;
+
+import adf.agent.action.ambulance.ActionLoad;
+import adf.agent.action.ambulance.ActionRescue;
+import adf.agent.action.ambulance.ActionUnload;
+import adf.agent.action.common.ActionMove;
+import adf.agent.info.AgentInfo;
+import adf.agent.info.ScenarioInfo;
+import adf.agent.info.WorldInfo;
+import adf.agent.module.ModuleManager;
+import adf.component.extaction.ExtAction;
+import adf.component.module.algorithm.PathPlanning;
+import adf.sample.SampleModuleKey;
+import rescuecore2.standard.entities.*;
+import rescuecore2.worldmodel.EntityID;
+
+import java.util.List;
+
+public class ActionTransport2 extends ExtAction {
+
+    private Human target;
+
+    public ActionTransport2(AgentInfo agentInfo, WorldInfo worldInfo, ScenarioInfo scenarioInfo, ModuleManager moduleManager) {
+        super(agentInfo, worldInfo, scenarioInfo, moduleManager);
+        this.target = null;
+    }
+
+    @Override
+    public ExtAction setTarget(EntityID... targets) {
+        if(targets != null) {
+            StandardEntity entity = this.worldInfo.getEntity(targets[0]);
+            if(entity instanceof Human) {
+                this.target = (Human)entity;
+            }
+        }
+        return this;
+    }
+
+    @Override
+    public ExtAction calc() {
+        this.result = null;
+        if(this.target == null) {
+            return this;
+        }
+        if(this.target.getPosition().equals(this.agentInfo.getID())) {
+            if(agentInfo.getPositionArea().getStandardURN().equals(StandardEntityURN.REFUGE)) {
+                this.result = new ActionUnload();
+            }
+            else {
+                if(this.target.isHPDefined() && this.target.getHP() == 0) {
+                    this.result = new ActionUnload();
+                } else {
+                    PathPlanning pathPlanning = this.moduleManager.getModule(SampleModuleKey.AMBULANCE_MODULE_PATH_PLANNING);
+                    pathPlanning.setFrom(agentInfo.getPosition()).setDestination(this.worldInfo.getEntityIDsOfType(StandardEntityURN.REFUGE));
+                    List<EntityID> path = pathPlanning.calc().getResult();
+                    if (path != null) {
+                        this.result = new ActionMove(path);
+                    }
+                }
+            }
+        }
+        else {
+            if(this.target.isHPDefined() && this.target.getHP() == 0) {
+                this.result = null;
+            } else if (target.getPosition().equals(agentInfo.getPosition())) {
+                if ((target instanceof Civilian) && target.getBuriedness() == 0 && !(agentInfo.getPositionArea() instanceof Refuge)) {
+                    this.result = new ActionLoad(target.getID());
+                } else if (target.getBuriedness() > 0) {
+                    this.result = new ActionRescue(target.getID());
+                }
+            } else {
+                PathPlanning pathPlanning = this.moduleManager.getModule(SampleModuleKey.AMBULANCE_MODULE_PATH_PLANNING);
+                pathPlanning.setFrom(agentInfo.getPosition()).setDestination(target.getPosition());
+                List<EntityID> path = pathPlanning.calc().getResult();
+                if (path != null) {
+                    this.result = new ActionMove(path);
+                }
+            }
+        }
+        return this;
+    }
+
+}
