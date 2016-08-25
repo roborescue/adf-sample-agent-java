953805efff86f3a55d9b719fc9c5d3f360a2c31c
URANOS-AIT <uranos.maslab@gmail.com>
2016-08-26 02:07:51 +0900

    update

--- src/adf/sample/module/complex/SampleVictimSelector.java
+++ src/adf/sample/module/complex/SampleVictimSelector.java
@@ -1,17 +1,18 @@
 package adf.sample.module.complex;
 
+import adf.agent.communication.MessageManager;
 import adf.agent.debug.DebugData;
 import adf.agent.info.AgentInfo;
 import adf.agent.info.ScenarioInfo;
 import adf.agent.info.WorldInfo;
 import adf.agent.module.ModuleManager;
 import adf.agent.precompute.PrecomputeData;
-import adf.sample.SampleModuleKey;
+import adf.component.communication.CommunicationMessage;
 import adf.component.module.algorithm.Clustering;
 import adf.component.module.complex.HumanSelector;
+import adf.sample.SampleModuleKey;
 import rescuecore2.standard.entities.Human;
 import rescuecore2.standard.entities.StandardEntity;
-import rescuecore2.standard.entities.StandardEntityURN;
 import rescuecore2.worldmodel.EntityID;
 
 import java.util.ArrayList;
@@ -19,78 +20,103 @@ import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
 
+import static rescuecore2.standard.entities.StandardEntityURN.*;
+
 public class SampleVictimSelector extends HumanSelector {
 
     private EntityID result;
-    private int clusterIndex;
 
     public SampleVictimSelector(AgentInfo ai, WorldInfo wi, ScenarioInfo si, ModuleManager moduleManager, DebugData debugData) {
         super(ai, wi, si, moduleManager, debugData);
-        this.clusterIndex = -1;
+    }
+
+    @Override
+    public HumanSelector updateInfo(MessageManager messageManager) {
+        super.updateInfo(messageManager);
+        for(CommunicationMessage message : messageManager.getReceivedMessageList()) {
+            Class<? extends CommunicationMessage> messageClass = message.getClass();
+
+        }
+        return this;
     }
 
     @Override
     public HumanSelector calc() {
         Clustering clustering = this.moduleManager.getModule(SampleModuleKey.AMBULANCE_MODULE_CLUSTERING);
         if(clustering == null) {
-            this.result = this.failedClusteringCalc();
+            this.result = this.calcTargetInWorld();
             return this;
         }
-        if(this.clusterIndex == -1) {
-            this.clusterIndex = clustering.getClusterIndex(this.agentInfo.getID());
+        this.result = this.calcTargetInCluster(clustering);
+        if(this.result == null) {
+            this.result = this.calcTargetInWorld();
         }
-        Collection<StandardEntity> elements = clustering.getClusterEntities(this.clusterIndex);
+        return this;
+    }
+
+    private EntityID calcTargetInCluster(Clustering clustering) {
+        int clusterIndex = clustering.getClusterIndex(this.agentInfo.getID());
+        Collection<StandardEntity> elements = clustering.getClusterEntities(clusterIndex);
+        if(elements == null || elements.isEmpty()) {
+            return null;
+        }
 
         List<Human> targets = new ArrayList<>();
-        for (StandardEntity next : worldInfo.getEntitiesOfType(
-                StandardEntityURN.CIVILIAN,
-                StandardEntityURN.FIRE_BRIGADE,
-                StandardEntityURN.POLICE_FORCE,
-                StandardEntityURN.AMBULANCE_TEAM)
-                ) {
-            if (agentInfo.getID() == next.getID()) {
-                continue;
-            }
+        for (StandardEntity next : this.worldInfo.getEntitiesOfType(AMBULANCE_TEAM, FIRE_BRIGADE, POLICE_FORCE)) {
             Human h = (Human) next;
             if(elements.contains(this.worldInfo.getPosition(h)) || elements.contains(h)) {
-                if (h.isHPDefined()
-                        && h.isBuriednessDefined()
-                        && h.isDamageDefined()
-                        && h.isPositionDefined()
-                        && h.getHP() > 0
-                        && (h.getBuriedness() > 0 || h.getDamage() > 0)) {
+                if (h.isHPDefined() && h.isBuriednessDefined() && h.getHP() > 0 && h.getBuriedness() > 0) {
                     targets.add(h);
                 }
             }
         }
-        targets.sort(new DistanceSorter(this.worldInfo, this.agentInfo.getPositionArea()));
-        result = targets.isEmpty() ? this.failedClusteringCalc() : targets.get(0).getID();
-        return this;
+        for (StandardEntity next : this.worldInfo.getEntitiesOfType(CIVILIAN)) {
+            Human h = (Human) next;
+            if (elements.contains(this.worldInfo.getPosition(h)) || elements.contains(h)) {
+                if (h.isHPDefined() && h.getHP() > 0) {
+                    if (h.isBuriednessDefined() && h.getBuriedness() > 0) {
+                        targets.add(h);
+                    } else if (h.isDamageDefined() && h.getDamage() > 0
+                            && this.worldInfo.getPosition(h).getStandardURN() != REFUGE) {
+                        targets.add(h);
+                    }
+                }
+            }
+        }
+        if(targets.size() > 0) {
+            targets.sort(new DistanceSorter(this.worldInfo, this.agentInfo.getPositionArea()));
+            return targets.get(0).getID();
+        }
+        return null;
     }
 
-    private EntityID failedClusteringCalc() {
+    private EntityID calcTargetInWorld() {
         List<Human> targets = new ArrayList<>();
-        for (StandardEntity next : worldInfo.getEntitiesOfType(
-                StandardEntityURN.CIVILIAN,
-                StandardEntityURN.FIRE_BRIGADE,
-                StandardEntityURN.POLICE_FORCE,
-                StandardEntityURN.AMBULANCE_TEAM)
-                ) {
-            Human h = (Human)next;
-            if (agentInfo.getID() == h.getID()) {
+        for (StandardEntity next : this.worldInfo.getEntitiesOfType(AMBULANCE_TEAM, FIRE_BRIGADE, POLICE_FORCE)) {
+            Human h = (Human) next;
+            if(this.agentInfo.getID().getValue() == h.getID().getValue()) {
                 continue;
             }
-            if (h.isHPDefined()
-                    && h.isBuriednessDefined()
-                    && h.isDamageDefined()
-                    && h.isPositionDefined()
-                    && h.getHP() > 0
-                    && (h.getBuriedness() > 0 || h.getDamage() > 0)) {
+            if (h.isHPDefined() && h.isBuriednessDefined() && h.getHP() > 0 && h.getBuriedness() > 0) {
                 targets.add(h);
             }
         }
-        targets.sort(new DistanceSorter(this.worldInfo, this.agentInfo.getPositionArea()));
-        return targets.isEmpty() ? null : targets.get(0).getID();
+        for (StandardEntity next : this.worldInfo.getEntitiesOfType(CIVILIAN)) {
+            Human h = (Human) next;
+            if (h.isHPDefined() && h.getHP() > 0) {
+                if (h.isBuriednessDefined() && h.getBuriedness() > 0) {
+                    targets.add(h);
+                } else if (h.isDamageDefined() && h.getDamage() > 0
+                        && this.worldInfo.getPosition(h).getStandardURN() != REFUGE) {
+                    targets.add(h);
+                }
+            }
+        }
+        if(targets.size() > 0) {
+            targets.sort(new DistanceSorter(this.worldInfo, this.agentInfo.getPositionArea()));
+            return targets.get(0).getID();
+        }
+        return null;
     }
 
     @Override
@@ -120,7 +146,7 @@ public class SampleVictimSelector extends HumanSelector {
         private StandardEntity reference;
         private WorldInfo worldInfo;
 
-        public DistanceSorter(WorldInfo wi, StandardEntity reference) {
+        DistanceSorter(WorldInfo wi, StandardEntity reference) {
             this.reference = reference;
             this.worldInfo = wi;
         }
