51510edca29867617059954b9417276da386ce9a
URANOS-AIT <uranos.maslab@gmail.com>
2016-09-20 16:21:50 +0900

    update

--- src/adf/sample/control/SampleControlAmbulance.java
+++ src/adf/sample/control/SampleControlAmbulance.java
@@ -13,14 +13,8 @@ import adf.agent.module.ModuleManager;
 import adf.agent.precompute.PrecomputeData;
 import adf.component.communication.CommunicationMessage;
 import adf.component.control.ControlAmbulance;
-import org.dom4j.Entity;
-import rescuecore2.messages.Message;
-import rescuecore2.standard.entities.AmbulanceTeam;
-import rescuecore2.standard.entities.Human;
-import rescuecore2.standard.entities.StandardEntity;
-import rescuecore2.standard.entities.StandardEntityURN;
+import rescuecore2.standard.entities.*;
 import rescuecore2.worldmodel.EntityID;
-import sample.DistanceSorter;
 
 import java.util.*;
 
@@ -32,65 +26,59 @@ public class SampleControlAmbulance extends ControlAmbulance {
 
     @Override
     public void think(AgentInfo agentInfo, WorldInfo worldInfo, ScenarioInfo scenarioInfo, ModuleManager moduleManager, MessageManager messageManager, DevelopData debugData) {
-        Collection<EntityID> commanders = worldInfo.getEntityIDsOfType(StandardEntityURN.AMBULANCE_CENTRE);
         Collection<EntityID> refuges = worldInfo.getEntityIDsOfType(StandardEntityURN.REFUGE);
         Set<EntityID> changedEntities = worldInfo.getChanged().getChangedEntities();
         Set<Human> priorities = new HashSet<>();
         Set<Human> victims = new HashSet<>();
         List<StandardEntity> agents = new ArrayList<>(worldInfo.getEntitiesOfType(StandardEntityURN.AMBULANCE_TEAM));
-        for(CommunicationMessage message : messageManager.getReceivedMessageList(MessageCivilian.class)) {
-            MessageCivilian mc = (MessageCivilian)message;
-            if(!changedEntities.contains(mc.getAgentID())){
-                MessageUtil.reflectMessage(worldInfo, mc);
-            }
+        for (CommunicationMessage message : messageManager.getReceivedMessageList(MessageCivilian.class)) {
+            MessageCivilian mc = (MessageCivilian) message;
+            MessageUtil.reflectMessage(worldInfo, mc);
         }
-        for(CommunicationMessage message : messageManager.getReceivedMessageList()) {
-            Class<? extends CommunicationMessage> messageClass = message.getClass();
-            if(messageClass == CommandAmbulance.class) {
-                CommandAmbulance command = (CommandAmbulance)message;
-                if(!commanders.contains(command.getSenderID())) {
-                    if(command.getAction() == CommandAmbulance.ACTION_RESCUE) {
-                        StandardEntity entity = worldInfo.getEntity(command.getTargetID());
-                        if(entity instanceof Human) {
-                            Human human = (Human)entity;
-                            if(human.isHPDefined() && human.isBuriednessDefined()
-                                    && human.getHP() > 0 && human.getBuriedness() > 0) {
-                                if(human.getStandardURN() != StandardEntityURN.CIVILIAN) {
-                                    priorities.add(human);
-                                } else {
-                                    victims.add(human);
-                                }
-                            }
-                        }
-                    } else if(command.getAction() == CommandAmbulance.ACTION_LOAD) {
-                        StandardEntity entity = worldInfo.getEntity(command.getTargetID());
-                        if(entity instanceof Human) {
-                            Human human = (Human)entity;
-                            if(human.isHPDefined() && human.getHP() > 0) {
+        for (CommunicationMessage message : messageManager.getReceivedMessageList(CommandAmbulance.class)) {
+            CommandAmbulance command = (CommandAmbulance) message;
+            if (command.isBroadcast()) {
+                if (command.getAction() == CommandAmbulance.ACTION_RESCUE) {
+                    StandardEntity entity = worldInfo.getEntity(command.getTargetID());
+                    if (entity instanceof Human) {
+                        Human human = (Human) entity;
+                        if (human.isHPDefined() && human.isBuriednessDefined()
+                                && human.getHP() > 0 && human.getBuriedness() > 0) {
+                            if (human.getStandardURN() != StandardEntityURN.CIVILIAN) {
+                                priorities.add(human);
+                            } else {
                                 victims.add(human);
-
                             }
                         }
                     }
-                }
-            } else if(messageClass == MessageAmbulanceTeam.class) {
-                MessageAmbulanceTeam mat = (MessageAmbulanceTeam)message;
-                if(!changedEntities.contains(mat.getAgentID())){
-                    MessageUtil.reflectMessage(worldInfo, mat);
-                }
-                if(mat.getAgentID().getValue() == mat.getSenderID().getValue()) {
-                    AmbulanceTeam ambulance = (AmbulanceTeam)worldInfo.getEntity(mat.getAgentID());
-                    if(mat.getBuriedness() > 0) {
-                        priorities.add(ambulance);
-                        agents.remove(ambulance);
-                    } else if(mat.getAction() == MessageAmbulanceTeam.ACTION_RESCUE) {
-                        agents.remove(ambulance);
-                    } else if(mat.getAction() == MessageAmbulanceTeam.ACTION_LOAD) {
-                        agents.remove(ambulance);
-                    } else if(mat.getAction() == MessageAmbulanceTeam.ACTION_MOVE) {
-                        if(refuges.contains(mat.getTargetID())) {
-                            agents.remove(ambulance);
+                } else if (command.getAction() == CommandAmbulance.ACTION_LOAD) {
+                    StandardEntity entity = worldInfo.getEntity(command.getTargetID());
+                    if (entity instanceof Civilian) {
+                        Civilian civilian = (Civilian) entity;
+                        if (civilian.isHPDefined() && civilian.getHP() > 0) {
+                            victims.add(civilian);
                         }
+                    }
+                }
+            }
+        }
+        for (CommunicationMessage message : messageManager.getReceivedMessageList(MessageAmbulanceTeam.class)) {
+            MessageAmbulanceTeam mat = (MessageAmbulanceTeam) message;
+            if (!changedEntities.contains(mat.getAgentID())) {
+                MessageUtil.reflectMessage(worldInfo, mat);
+            }
+            if (mat.getAgentID().getValue() == mat.getSenderID().getValue()) {
+                AmbulanceTeam ambulance = (AmbulanceTeam) worldInfo.getEntity(mat.getAgentID());
+                if (mat.getBuriedness() > 0) {
+                    priorities.add(ambulance);
+                    agents.remove(ambulance);
+                } else if (mat.getAction() == MessageAmbulanceTeam.ACTION_RESCUE) {
+                    agents.remove(ambulance);
+                } else if (mat.getAction() == MessageAmbulanceTeam.ACTION_LOAD) {
+                    agents.remove(ambulance);
+                } else if (mat.getAction() == MessageAmbulanceTeam.ACTION_MOVE) {
+                    if (refuges.contains(mat.getTargetID())) {
+                        agents.remove(ambulance);
                     }
                 }
             }
