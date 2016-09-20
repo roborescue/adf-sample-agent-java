8d58c5c79766df9ea34fe3ea32356bbd0e90dc58
Shunki <tkmnet@users.noreply.github.com>
2016-09-20 12:31:12 +0900

    Update .update-library.sh

--- .update-library.sh
+++ .update-library.sh
@@ -18,8 +18,8 @@ fi
 cd `dirname $0`
 cd ./library/rescue/adf
 
-$WGET 'adf-core.jar' "https://github.com/RCRS-ADF/core/raw/jar/build/libs/adf-core.jar?$$"
+$WGET 'adf-core.jar' "https://raw.githubusercontent.com/RCRS-ADF/core/jar/build/libs/adf-core.jar?$$"
 # $WGET 'adf-modules.jar' "https://raw.githubusercontent.com/RCRS-ADF/modules/master/build/libs/adf-modules.jar?$$"
 cd sources
-$WGET 'adf-core-sources.jar' "https://github.com/RCRS-ADF/core/raw/jar/build/libs/adf-core-sources.jar?$$"
+$WGET 'adf-core-sources.jar' "https://raw.githubusercontent.com/RCRS-ADF/core/jar/build/libs/adf-core-sources.jar?$$"
 # $WGET 'adf-modules-sources.jar' "https://raw.githubusercontent.com/RCRS-ADF/modules/master/build/libs/adf-modules-sources.jar?$$"
