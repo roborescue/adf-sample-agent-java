3fbad1eba9dcf46666bddc0e645a8f43973bb06d
Tomoki-Mizuno <buddyfightlovenge27@icloud.com>
2025-09-26 14:38:22 +0900

    Update SampleRoadDetector.java
    Fixed a bug where the Police Force would stop clearing operations.

--- src/main/java/sample_team/module/complex/SampleRoadDetector.java
+++ src/main/java/sample_team/module/complex/SampleRoadDetector.java
@@ -1,5 +1,12 @@
 package sample_team.module.complex;
 
+import static rescuecore2.standard.entities.StandardEntityURN.AMBULANCE_TEAM;
+import static rescuecore2.standard.entities.StandardEntityURN.CIVILIAN;
+import static rescuecore2.standard.entities.StandardEntityURN.FIRE_BRIGADE;
+import static rescuecore2.standard.entities.StandardEntityURN.GAS_STATION;
+import static rescuecore2.standard.entities.StandardEntityURN.POLICE_FORCE;
+import static rescuecore2.standard.entities.StandardEntityURN.REFUGE;
+import adf.core.agent.communication.MessageManager;
 import adf.core.agent.develop.DevelopData;
 import adf.core.agent.info.AgentInfo;
 import adf.core.agent.info.ScenarioInfo;
@@ -9,157 +16,163 @@ import adf.core.component.module.algorithm.Clustering;
 import adf.core.component.module.algorithm.PathPlanning;
 import adf.core.component.module.complex.RoadDetector;
 import adf.core.debug.DefaultLogger;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import org.apache.log4j.Logger;
-import rescuecore2.standard.entities.*;
+import rescuecore2.standard.entities.Area;
+import rescuecore2.standard.entities.Human;
+import rescuecore2.standard.entities.StandardEntity;
+import rescuecore2.standard.entities.StandardEntityURN;
 import rescuecore2.worldmodel.EntityID;
-import adf.core.agent.communication.MessageManager;
 
 public class SampleRoadDetector extends RoadDetector {
 
-    private PathPlanning pathPlanning;
-    private Clustering clustering;
-    private Logger logger;
+  private Set<Area> openedAreas = new HashSet<>();
+  private Clustering clustering;
+  private PathPlanning pathPlanning;
 
-    // key is road ID, value is priority
-    private Map<EntityID, Integer> tasks;
-    // a Set that holds the ID of roads that have been cleared
-    private Set<EntityID> completedTasks;
-    // a Set that holds all entity IDs within the assigned area (cluster)
-    private Set<EntityID> myCluster;
-    // ID of the road to be cleared
-    private EntityID result;
+  private EntityID result;
+  private Logger logger;
 
-    // constructor
-    public SampleRoadDetector(AgentInfo ai, WorldInfo wi, ScenarioInfo si, ModuleManager moduleManager, DevelopData developData) {
-        super(ai, wi, si, moduleManager, developData);
-
-        this.logger = DefaultLogger.getLogger(agentInfo.me());
-        this.pathPlanning = moduleManager.getModule("SampleRoadDetector.PathPlanning", "adf.impl.module.algorithm.DijkstraPathPlanning");
-        this.clustering = moduleManager.getModule("SampleRoadDetector.Clustering", "adf.impl.module.algorithm.KMeansClustering");
-
-        registerModule(this.clustering);
-        registerModule(this.pathPlanning);
-
-        this.tasks = new HashMap<>();
-        this.completedTasks = new HashSet<>();
-        this.myCluster = new HashSet<>();
-    }
+  public SampleRoadDetector(AgentInfo ai, WorldInfo wi, ScenarioInfo si, ModuleManager moduleManager, DevelopData developData) {
+    super(ai, wi, si, moduleManager, developData);
+    logger = DefaultLogger.getLogger(agentInfo.me());
+    this.pathPlanning = moduleManager.getModule(
+        "SampleRoadDetector.PathPlanning",
+        "adf.impl.module.algorithm.DijkstraPathPlanning");
+    this.clustering = moduleManager.getModule("SampleRoadDetector.Clustering",
+        "adf.impl.module.algorithm.KMeansClustring");
+    registerModule(this.clustering);
+    registerModule(this.pathPlanning);
+    this.result = null;
+  }
 
 
-    @Override
-    public RoadDetector preparate() {
-        super.preparate();
-        // be called only once
-        if (this.getCountPrecompute() > 1) { return this; }
+  @Override
+  public RoadDetector updateInfo(MessageManager messageManager) {
+    logger.debug("Time:" + agentInfo.getTime());
+    super.updateInfo(messageManager);
 
-        // get the entity ID of your own area
-        int myClusterIndex = this.clustering.getClusterIndex(this.agentInfo.getID());
-        if (myClusterIndex != -1) {
-            Collection<StandardEntity> clusterEntities = this.clustering.getClusterEntities(myClusterIndex);
-            if(clusterEntities != null) {
-                this.myCluster.addAll(
-                    clusterEntities.stream().map(StandardEntity::getID).collect(Collectors.toSet())
-                );
-            }
+    // adding a road to the list only when its clearance is complete, rather than upon arrival, prevents clearing operations from stopping
+    for (EntityID id : this.worldInfo.getChanged().getChangedEntities()) {
+      StandardEntity entity = this.worldInfo.getEntity(id);
+      if (entity instanceof Road) {
+        Road road = (Road) entity;
+        if (!road.isBlockadesDefined() || road.getBlockades().isEmpty()) {
+          this.openedAreas.add(id);
         }
-        return this;
+      }
     }
 
-    // information update
-    @Override
-    public RoadDetector updateInfo(MessageManager messageManager) {
-        super.updateInfo(messageManager);
-        logger.debug("Time:" + agentInfo.getTime());
+    return this;
+  }
 
-        // update tasks based on information
-        updateTasksWithPerception();
-        addClusterRoadsToTasksAsNeeded();
 
-        return this;
-    }
-
-    // calculate clearing target
-    @Override
-    public RoadDetector calc() {
-      // reset result
+  @Override
+  public RoadDetector calc() {
+    EntityID positionID = this.agentInfo.getPosition();
+    StandardEntity currentPosition = worldInfo.getEntity(positionID);
+    openedAreas.add((Area) currentPosition);
+    if (positionID.equals(result)) {
+      logger.debug("reach to " + currentPosition + " resetting target");
       this.result = null;
-      EntityID bestTarget = null;
-      int minPriority = Integer.MAX_VALUE;
-      int minDistance = Integer.MAX_VALUE;
+    }
 
-      for (Map.Entry<EntityID, Integer> task : this.tasks.entrySet()) {
-        EntityID currentTarget = task.getKey();
-        int currentPriority = task.getValue();
-
-        // ignore if already cleared
-        if (this.completedTasks.contains(currentTarget)) {
-          continue;
-        }
-
-        // compare priority
-        if (currentPriority < minPriority) {
-          minPriority = currentPriority;
-          bestTarget = currentTarget;
-          minDistance = this.worldInfo.getDistance(this.agentInfo.getID(), bestTarget);
-        } else if (currentPriority == minPriority) {
-          // if priority is the same, compare distance
-          int currentDistance = this.worldInfo.getDistance(this.agentInfo.getID(), currentTarget);
-          if (currentDistance < minDistance) {
-            bestTarget = currentTarget;
-            minDistance = currentDistance;
-          }
-        }
+    if (this.result == null) {
+      HashSet<Area> currentTargets = calcTargets();
+      logger.debug("Targets: " + currentTargets);
+      if (currentTargets.isEmpty()) {
+        this.result = null;
+        return this;
       }
-
-      if (bestTarget != null) {
-        this.result = bestTarget;
-      } else {
-        if(!this.completedTasks.isEmpty()) {
-          this.completedTasks.clear();
-        }
+      this.pathPlanning.setFrom(positionID);
+      this.pathPlanning.setDestination(toEntityIds(currentTargets));
+      List<EntityID> path = this.pathPlanning.calc().getResult();
+      if (path != null && path.size() > 0) {
+        this.result = path.get(path.size() - 1);
       }
-      return this;
+      logger.debug("Selected Target: " + this.result);
     }
-
-    // update tasks based on perception
-    private void updateTasksWithPerception() {
-        // loop through changed entities
-        for (EntityID id : this.worldInfo.getChanged().getChangedEntities()) {
-            StandardEntity entity = this.worldInfo.getEntity(id);
-            // if the entity is a road
-            if (entity instanceof Road) {
-                Road road = (Road) entity;
-                // if there are blockades on the road
-                if (road.isBlockadesDefined() && !road.getBlockades().isEmpty()) {
-                    // add to tasks with priority 1
-                    this.tasks.put(road.getID(), 1);
-                    // remove from completed tasks if it was there
-                    this.completedTasks.remove(road.getID());
-                } else {
-                    // add to completed tasks
-                    this.completedTasks.add(road.getID());
-                }
-            }
-        }
-    }
+    return this;
+  }
 
 
-    private void addClusterRoadsToTasksAsNeeded() {
-        if(this.myCluster == null) return;
-        for(EntityID id : this.myCluster) {
-            // if road entity
-            if(this.worldInfo.getEntity(id) instanceof Road) {
-                // add to tasks with priority 8
-                this.tasks.putIfAbsent(id, 8);
-            }
-        }
+  private Collection<EntityID>
+      toEntityIds(Collection<? extends StandardEntity> entities) {
+    ArrayList<EntityID> eids = new ArrayList<>();
+    for (StandardEntity standardEntity : entities) {
+      eids.add(standardEntity.getID());
     }
+    return eids;
+  }
 
-    // return the ID of the road to be cleared
-    @Override
-    public EntityID getTarget() {
-        return this.result;
+
+  private HashSet<Area> calcTargets() {
+    HashSet<Area> targetAreas = new HashSet<>();
+    for (StandardEntity e : this.worldInfo.getEntitiesOfType(REFUGE,
+        GAS_STATION)) {
+      targetAreas.add((Area) e);
     }
+    for (StandardEntity e : this.worldInfo.getEntitiesOfType(CIVILIAN,
+        AMBULANCE_TEAM, FIRE_BRIGADE, POLICE_FORCE)) {
+      if (isValidHuman(e)) {
+        Human h = (Human) e;
+        targetAreas.add((Area) worldInfo.getEntity(h.getPosition()));
+      }
+    }
+    HashSet<Area> inClusterTarget = filterInCluster(targetAreas);
+    inClusterTarget.removeAll(openedAreas);
+    return inClusterTarget;
+  }
+
+
+  private HashSet<Area> filterInCluster(HashSet<Area> targetAreas) {
+    int clusterIndex = clustering.getClusterIndex(this.agentInfo.getID());
+    HashSet<Area> clusterTargets = new HashSet<>();
+    HashSet<StandardEntity> inCluster = new HashSet<>(
+        clustering.getClusterEntities(clusterIndex));
+    for (Area target : targetAreas) {
+      if (inCluster.contains(target))
+        clusterTargets.add(target);
+
+    }
+    return clusterTargets;
+  }
+
+
+  @Override
+  public EntityID getTarget() {
+    return this.result;
+  }
+
+
+  private boolean isValidHuman(StandardEntity entity) {
+    if (entity == null)
+      return false;
+    if (!(entity instanceof Human))
+      return false;
+
+    Human target = (Human) entity;
+    if (!target.isHPDefined() || target.getHP() == 0)
+      return false;
+    if (!target.isPositionDefined())
+      return false;
+    if (!target.isDamageDefined() || target.getDamage() == 0)
+      return false;
+    if (!target.isBuriednessDefined())
+      return false;
+
+    StandardEntity position = worldInfo.getPosition(target);
+    if (position == null)
+      return false;
+
+    StandardEntityURN positionURN = position.getStandardURN();
+    if (positionURN == REFUGE || positionURN == AMBULANCE_TEAM)
+      return false;
+
+    return true;
+  }
 }
